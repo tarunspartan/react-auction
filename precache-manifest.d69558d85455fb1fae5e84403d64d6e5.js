b0373373e5a6350a534d01f5b476d6b6f13d85fb
Tarun M <tarunspartan1234@outlook.com>
2019-12-15 22:15:11 +0530

    Updates

---
+++ precache-manifest.d69558d85455fb1fae5e84403d64d6e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b006601a18652b05ad5c2de9ccf2ac7",
+    "url": "/react-auction/index.html"
+  },
+  {
+    "revision": "4a5d056642b1d287ad12",
+    "url": "/react-auction/static/css/main.a4df57c9.chunk.css"
+  },
+  {
+    "revision": "9cacc6c4450ab301a82c",
+    "url": "/react-auction/static/js/2.fd455b88.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/react-auction/static/js/2.fd455b88.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4a5d056642b1d287ad12",
+    "url": "/react-auction/static/js/main.83affd0a.chunk.js"
+  },
+  {
+    "revision": "797b6256f01868edef77",
+    "url": "/react-auction/static/js/runtime-main.c0664e15.js"
+  }
+]);
